d8d25d263c4940b69c0e180fd6be2a6cb2415342
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-19 11:34:09 +0530

    Create App.js

---
+++ MiniProjects(WebDev)/Weather-APP-Dynamic/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./Components/WeatherApp/WeatherApp.jsx";
+import WeatherApp from "./Components/WeatherApp/WeatherApp.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
